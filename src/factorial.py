d654b047927de20d70bfbda5a04522c5abb66f29
guilherme <guilherme.rezende@redacaonline.com.br>
2023-08-01 20:11:20 -0300

    efficent tail recursive algorithm

--- src/factorial.py
+++ src/factorial.py
@@ -4,6 +4,13 @@ def factorial(n):
     print(f"-> factorial({n}) returns {return_value}")
     return return_value
 
+def factorial_tail_recursive(n, acc=1):
+    # acc already does the operations without needing to transverse the n sequence
+    print(f"factorial_tail_recursive() called with n = {n}, and acc = {acc}")
+    return_value = acc if n <= 1 else factorial_tail_recursive(n - 1, n * acc)
+    print(f"-> factorial_tail_recursive({n}) returns {return_value}")
+    return return_value
+
 if __name__ == "__main__":
     # Get an integer input from the user
     user_input = input("Enter an integer: ")
@@ -11,6 +18,6 @@ if __name__ == "__main__":
     try:
         # Convert the input string to an integer
         user_integer = int(user_input)
-        factorial_result = factorial(user_integer)
+        factorial_result = factorial_tail_recursive(user_integer)
     except ValueError:
         print("Invalid input. Please enter a valid integer.")
